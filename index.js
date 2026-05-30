ef8e4a6fc164d64cb1de17c162d69fd163abad32
Topu-Again <mdtanbirtopu@gmail.com>
2026-05-30 22:29:31 +0600

    Adventure Time Update

---
+++ index.js
@@ -0,0 +1,8 @@
+function finn() {
+    document.getElementById('finn').style.display = "block";
+    document.getElementById('choosingPanel').style.display = "none";
+}
+function jake() {
+    document.getElementById('jake').style.display = "block";
+    document.getElementById('choosingPanel').style.display = "none";
+}
